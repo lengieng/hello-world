f05048e6a683ac45d4a28eb2e24c4c4a8be704e5
Lengieng <ing.lengieng@gmail.com>
2016-03-22 20:43:13 -0300

    Added condition for POST request
    Added condition for POST request method

--- app.php
+++ app.php
@@ -6,6 +6,8 @@
         if ($_SERVER["REQUEST_METHOD"] == "GET") {
             if (isset($_GET["name"]))
                 echo "Hi, " . $_GET["name"];
+        } else {
+            // Handle POST request here
         }
         ?>
     </body>
